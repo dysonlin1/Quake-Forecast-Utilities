9b92603989c3598242728b97372808f7c9572cdc
林湧森 (Dyson Lin) <dysonlin@gmail.com>
2017-05-10 00:34:08 +0800

    station.py

---
+++ station.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# station.py
+
+station = {}
+
+station['台灣各站'] = {
+    'Chinese': '台灣各站', 
+    'Japanese': '台湾各局', 
+    'English': 'Taiwan Stations '}
+
+station['南非德本站'] = {
+    'Chinese': '南非德本站', 
+    'Japanese': '南アフリカ ダーバン局', 
+    'English': 'Durban, South Africa Station '}
+
+station['義大利法恩扎站'] = {
+    'Chinese': '義大利法恩扎站', 
+    'Japanese': 'イタリア ファエンツァ局', 
+    'English': 'Faenza, Italy Station '}
+
+station['南投草屯站'] = {
+    'Chinese': '南投草屯站', 
+    'Japanese': '南投草屯局', 
+    'English': 'Nantou Caotun Station '}
+
+station['台南熱心站'] = {
+    'Chinese': '台南熱心站', 
+    'Japanese': '台南熱心局', 
+    'English': 'Tainan Hot Heart Station '}
+
+station['台南喜福站'] = {
+    'Chinese': '台南喜福站', 
+    'Japanese': '台南喜福局', 
+    'English': 'Tainan Joy & Luck Station '}
+
+station['台南新生站'] = {
+    'Chinese': '台南新生站', 
+    'Japanese': '台南新生局', 
+    'English': 'Tainan Xinsheng Station '}
+
+station['高雄站'] = {
+    'Chinese': '高雄站', 
+    'Japanese': '高雄局', 
+    'English': 'Kaohsiung Station '}
+
+station['台中潭子站'] = {
+    'Chinese': '台中潭子站', 
+    'Japanese': '台中潭子局', 
+    'English': 'Taichung Tantzu Station '}
+
+station['桃園站'] = {
+    'Chinese': '桃園站', 
+    'Japanese': '桃園局', 
+    'English': 'Taoyuan Station '}
+
+station['宜蘭站'] = {
+    'Chinese': '宜蘭站', 
+    'Japanese': '宜蘭局', 
+    'English': 'Yilan Station '}
+
+station['台中大里站'] = {
+    'Chinese': '台中大里站', 
+    'Japanese': '台中大里局', 
+    'English': 'Taichung Dali Station '}
+
+station['智利伊基克站'] = {
+    'Chinese': '智利伊基克站', 
+    'Japanese': 'チリ イキケ局', 
+    'English': 'Iquique, Chile Station '}
+
+station['義大利卡爾塔尼塞塔站'] = {
+    'Chinese': '義大利卡爾塔尼塞塔站', 
+    'Japanese': 'イタリア カルタニッセッタ局', 
+    'English': 'Caltanissetta, Italy Station '}
+
+station['紐西蘭奧克蘭站'] = {
+    'Chinese': '紐西蘭奧克蘭站', 
+    'Japanese': 'ニュージーランド オークランド局', 
+    'English': 'Auckland, New Zealand Station '}
+
