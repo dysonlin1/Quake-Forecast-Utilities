8abee764a5a64c4a1a73ad2d3a10f943a9d0c150
林湧森 (Dyson Lin) <dysonlin@gmail.com>
2017-08-10 03:23:53 +0800

    post_to_Facabook

--- quake_forecast.py
+++ quake_forecast.py
@@ -779,14 +779,16 @@ def post_to_Facebook(tab_number):
         pyautogui.scroll(-700) # scroll down
         time.sleep(2)
         
-        for i in range(0, 15):
+        #for i in range(0, 15):
+        for i in range(0, 13):
             pyautogui.press('tab')
             #time.sleep(1)
         pyautogui.press('enter')
         time.sleep(1)
         #pyautogui.scroll(-100) # scroll down
     elif tab_number is 3:
-        for i in range(0, 23):
+        #for i in range(0, 23):
+        for i in range(0, 14):
             pyautogui.press('tab')
         pyautogui.press('enter')
         time.sleep(1)
